4136c2719d2346f5eac1f0fbb2aa0c81a0c67bdf
LeCanardNoir <dev.lecanardnoir@gmail.com>
2023-10-09 13:15:15 -0400

    INIT: Project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿// List.cpp : définit le point d'entrée de l'application.
+//
+
+#include "List.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
