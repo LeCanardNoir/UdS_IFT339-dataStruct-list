6cb0624d39325e1ac68dc750352f1a2c19f96add
Bruno Pouliot <dev.lecanardnoir@gmail.com>
2023-10-11 17:43:46 -0400

    BUG: insert

--- listImpl.h
+++ listImpl.h
@@ -7,8 +7,8 @@
 //  Modifie par : Vincent Ducharme, Automne 2022
 //
 //  Devoir fait par
-//     Coéquipier 1 :
-//     Coéquipier 2 :
+//     Coéquipier 1 : Bruno Pouliot
+//     Coéquipier 2 : Gabriel Dumont-Hétu
 
 #ifndef listImpl_h
 #define listImpl_h
@@ -20,11 +20,32 @@
 template <typename TYPE>
 typename list<TYPE>::cellule* list<TYPE>::insert(cellule* c, const TYPE& VAL)
 {
+    //BUUUUUUUUUUUGGGGG
+    cellule nouv;
+    cellule* ptrNouv = &nouv;
+    ptrNouv->m_contenu = VAL;
+    prtNouv->m_prec = c->m_prec;
+    c->m_prec = prtNouv;
+    prtNouv->m_suiv = c;
+    if (prtNouv->m_prec) {
+        prtNouv->m_prec->m_suiv = prtNouv;
+    }
+    m_size++;
 }
 
 template <typename TYPE>
 typename list<TYPE>::cellule* list<TYPE>::erase(cellule* c)
 {
+    if (size() > 0) {
+        c->m_suiv->m_prec = c->m_prec;
+        if(c->m_prec)
+            c->m_prec->m_suiv = c->m_suiv;
+
+        m_size--;
+    }
+
+    return c;
+    
 }
 
 
@@ -79,6 +100,16 @@ list<TYPE>& list<TYPE>::operator=(const list<TYPE>& droite)
 template <typename TYPE>
 void list<TYPE>::reverse()
 {
+    if (m_debut) {
+        std::swap(m_debut->m_prec, m_apres.m_prec->m_suiv);
+        cellule* c = m_debut;
+        while (c) {
+            std::swap(c->m_suiv, c->m_prec);
+            //on va au suivant, mais vu qu'on vient de swapper prec et suiv, on va au "précédent"
+            c = c->m_prec;
+        }
+        std::swap(m_debut, m_apres.m_prec);
+    }
 }
 
 
--- main.cpp
+++ main.cpp
@@ -9,5 +9,24 @@ using namespace std;
 int main()
 {
 	cout << "Hello CMake." << endl;
+
+	list<int> l1;
+	l1.push_back(0);
+	l1.push_back(1);
+	l1.push_back(2);
+	l1.push_back(3);
+	l1.push_back(4);
+	l1.push_back(5);
+	l1.push_back(6);
+	l1.push_back(7);
+	l1.push_back(8);
+	l1.push_back(9);
+	l1.push_back(10);
+	l1.push_back(11);
+	l1.push_back(12);
+	l1.push_back(13);
+	afficher(cout, l1);
+
+
 	return 0;
 }
