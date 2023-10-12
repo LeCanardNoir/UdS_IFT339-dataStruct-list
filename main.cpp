0febf0ef3cc78bd92cd9448a65ae8fe669f89ccf
2160630 <104075924+2160630@users.noreply.github.com>
2023-10-12 10:25:53 -0400

    FCT tests

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿// List.cpp : définit le point d'entrée de l'application.
+// List.cpp : définit le point d'entrée de l'application.
 //
 
 #include "List.h"
@@ -11,20 +11,23 @@ int main()
 	cout << "Hello CMake." << endl;
 
 	list<int> l1;
-	l1.push_back(0);
-	l1.push_back(1);
-	l1.push_back(2);
-	l1.push_back(3);
-	l1.push_back(4);
-	l1.push_back(5);
-	l1.push_back(6);
-	l1.push_back(7);
-	l1.push_back(8);
-	l1.push_back(9);
-	l1.push_back(10);
-	l1.push_back(11);
 	l1.push_back(12);
-	l1.push_back(13);
+	l1.push_back(34);
+	l1.push_back(56);
+	l1.push_back(78);
+	l1.push_back(90);
+	afficher(cout, l1);
+
+	/*
+	l1.pop_back();
+	afficher(cout, l1);
+	
+	l1.pop_front();
+	l1.pop_front();
+	afficher(cout, l1);
+	//*/
+	
+	l1.reverse();
 	afficher(cout, l1);
 
 
