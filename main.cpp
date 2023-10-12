c6faf3716a929e1df72e8c6eecaeb85873224c89
Bruno Pouliot <dev.lecanardnoir@gmail.com>
2023-10-12 10:32:15 -0400

    PROGRESS:

--- main.cpp
+++ main.cpp
@@ -11,11 +11,13 @@ int main()
 	cout << "Hello CMake." << endl;
 	list<int> list_simple;
 	list<int> list_data({1,2,3,4,5,6,7,8,9,0});
+	afficher(cout, list_data);
+	cout << endl;
 	for (auto it = list_data.begin(); it != list_data.end(); it++) {
+		cout << *it << ", ";
+	}
+	for (auto it = list_data.rbegin(); it != list_data.rend(); it++) {
 		cout << *it << endl;
 	}
-	/*for (auto it = list_data.rbegin(); it != list_data.rend(); it++) {
-		cout << *it << endl;
-	}*/
 	return 0;
 }
